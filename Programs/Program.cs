0f4782f978ce0ccbf272dfb3fa8c32caef99ba22
Bondarenko Artem <Bondarenko@mail.ru>
2022-12-13 15:54:04 +0300

    начало написания кода

---
+++ Programs/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string[] array = { "Hello", "2", "world", ":-)"};
+int count = ArrayVerify(array);
+string[] resultArray = FillArray(array, count);
+
+int ArrayVerify(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+string[] FillArray(string[] array, int count)
